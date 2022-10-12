4aa02b5378d0b4363810483812e83e5782b3e88b
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-12 12:29:56 +0800

    测试

---
+++ stringify_test.ts
@@ -0,0 +1,24 @@
+import { ArrayToCircularLinkedList } from "./ArrayToCircularLinkedList.ts";
+import { createCircular } from "./createCircular.ts";
+import { ArrayToCircularDoublyTreeList } from "./ArrayToCircularDoublyTreeList.ts";
+import { teststringify } from "./teststringify.ts";
+
+Deno.test("stringify", () => {
+    const d: any[] = createCircular();
+    const origin = ArrayToCircularLinkedList(
+        Array.from({ length: 10 }, (_, i) => i + Math.random()),
+    );
+
+    teststringify(origin);
+
+    teststringify(d);
+
+    teststringify(
+        ArrayToCircularDoublyTreeList(
+            Array.from({ length: 10 }, (_, i) => i + Math.random()),
+        ),
+    );
+    const case2: any[][] = [[1]];
+    case2.push(case2);
+    teststringify(case2);
+});
--- teststringify.ts
+++ teststringify.ts
@@ -1,8 +1,5 @@
-import { ArrayToCircularLinkedList } from "./ArrayToCircularLinkedList.ts";
 import { circularStringify } from "./circularStringify.ts";
 import { cloneDeep } from "./cloneDeep.ts";
-import { createCircular } from "./createCircular.ts";
-import { ArrayToCircularDoublyTreeList } from "./ArrayToCircularDoublyTreeList.ts";
 import { replacerCustomizers } from "./replacerCustomizers.ts";
 export function teststringify(origin: any) {
     const cloned = cloneDeep(origin, ...replacerCustomizers);
@@ -13,21 +10,3 @@ export function teststringify(origin: any) {
     console.log("stringified", stringified);
     return stringified;
 }
-const d: any[] = createCircular();
-
-const origin = ArrayToCircularLinkedList(
-    Array.from({ length: 10 }, (_, i) => i + Math.random()),
-);
-
-teststringify(origin);
-
-teststringify(d);
-
-teststringify(
-    ArrayToCircularDoublyTreeList(
-        Array.from({ length: 10 }, (_, i) => i + Math.random()),
-    ),
-);
-const case2: any[][] = [[1]];
-case2.push(case2);
-teststringify(case2);
