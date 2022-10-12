088ddfcab5d5b6e5825c1417dae7a0017b5bc66f
masx200 <34191203+masx200@users.noreply.github.com>
2022-10-12 13:35:59 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -6,6 +6,7 @@ import {
 } from "https://deno.land/std@0.159.0/testing/asserts.ts";
 import { teststringify } from "./teststringify.ts";
 import { testparse } from "./testparse.ts";
+import { ArrayToCircularDoublyTreeList } from "./ArrayToCircularDoublyTreeList.ts";
 Deno.test("basic object", () => {
     const obj = {};
     //@ts-ignore
@@ -21,7 +22,7 @@ Deno.test("basic object", () => {
     assert(clone.a === clone); // -> true
     assertEquals(
         stringified,
-        `{"a":{"Symbol.reference":0},"Symbol.identity":0}`,
+        `{"a":{"Symbol.reference":0},"Symbol.identity":0}`
     );
 });
 Deno.test("basic array", () => {
@@ -49,6 +50,104 @@ Deno.test("basic array", () => {
     "Symbol.toStringTag": "Array",
     "length": 2,
     "Symbol.identity": 0
-}`,
+}`
+    );
+});
+Deno.test("basic tree list", () => {
+    const obj = ArrayToCircularDoublyTreeList(
+        Array.from({ length: 10 }, (_, i) => i + 100)
+    );
+    const stringified = teststringify(obj);
+    console.log(stringified);
+    console.log(obj);
+    const clone = testparse(stringified);
+    assert(clone !== obj);
+    assertEquals(clone, obj);
+
+    assertEquals(
+        stringified,
+        `{
+    "val": 100,
+    "left": {
+        "val": 109,
+        "left": {
+            "val": 108,
+            "left": {
+                "val": 107,
+                "left": {
+                    "val": 106,
+                    "left": {
+                        "val": 105,
+                        "left": {
+                            "val": 104,
+                            "left": {
+                                "val": 103,
+                                "left": {
+                                    "val": 102,
+                                    "left": {
+                                        "val": 101,
+                                        "left": {
+                                            "Symbol.reference": 0
+                                        },
+                                        "right": {
+                                            "Symbol.reference": 8
+                                        },
+                                        "Symbol.toStringTag": "TreeNode",
+                                        "Symbol.identity": 9
+                                    },
+                                    "right": {
+                                        "Symbol.reference": 7
+                                    },
+                                    "Symbol.toStringTag": "TreeNode",
+                                    "Symbol.identity": 8
+                                },
+                                "right": {
+                                    "Symbol.reference": 6
+                                },
+                                "Symbol.toStringTag": "TreeNode",
+                                "Symbol.identity": 7
+                            },
+                            "right": {
+                                "Symbol.reference": 5
+                            },
+                            "Symbol.toStringTag": "TreeNode",
+                            "Symbol.identity": 6
+                        },
+                        "right": {
+                            "Symbol.reference": 4
+                        },
+                        "Symbol.toStringTag": "TreeNode",
+                        "Symbol.identity": 5
+                    },
+                    "right": {
+                        "Symbol.reference": 3
+                    },
+                    "Symbol.toStringTag": "TreeNode",
+                    "Symbol.identity": 4
+                },
+                "right": {
+                    "Symbol.reference": 2
+                },
+                "Symbol.toStringTag": "TreeNode",
+                "Symbol.identity": 3
+            },
+            "right": {
+                "Symbol.reference": 1
+            },
+            "Symbol.toStringTag": "TreeNode",
+            "Symbol.identity": 2
+        },
+        "right": {
+            "Symbol.reference": 0
+        },
+        "Symbol.toStringTag": "TreeNode",
+        "Symbol.identity": 1
+    },
+    "right": {
+        "Symbol.reference": 9
+    },
+    "Symbol.toStringTag": "TreeNode",
+    "Symbol.identity": 0
+}`
     );
 });
